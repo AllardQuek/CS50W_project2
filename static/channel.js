aeae75e1adc0f0cf399c4f71da6c5776541ecc1c
Allard <AllardQuek@users.noreply.github.com>
2019-11-15 22:55:02 +0800

    cleaned channel.js

--- static/channel.js
+++ static/channel.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // configure button to consolidate user's message info 
     document.getElementById('send_message').onclick = () => {
         var display_name = localStorage.getItem('displayName');
         var content = document.getElementById('textarea').value;
-        var datetime = new Date().toLocaleString();    // https://stackoverflow.com/questions/10211145/getting-current-date-and-time-in-javascript
+        var datetime = new Date().toLocaleString();
         var channel_name = document.getElementById('channel_name').innerHTML;
 
+        // emit event to check message limit and store info server-side
         socket.emit('send_message', {'display_name': display_name, 'content': content, 'datetime': datetime, 'channel_name': channel_name});
     };
 });
